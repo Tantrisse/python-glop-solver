d4760d2e6ad827e42137668733a11ee78e771ace
CHANOVE Tristan <chanove.tristan@gmail.com>
2021-06-19 13:57:11 +0200

    Switch from file writing to http response via flask

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, request
+from glop_solver import solve_problem
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({'message': 'OK', 'status': 200})
+
+
+@app.route('/solve', methods=['POST'])
+def solve():
+    # TODO :
+    #   - check params
+    #   - call solver
+    #   - return response json
+    objectives = request.json['objectives']
+    constraints = request.json['constraints']
+    solution = solve_problem(objectives, constraints)
+    # print('solution : %s' % solution)
+    return jsonify({'message': 'OK', 'status': 200, 'data': solution})
--- glop_solver.py
+++ glop_solver.py
@@ -1,6 +1,3 @@
-import json
-import sys
-
 from ortools.linear_solver import pywraplp
 
 
@@ -36,43 +33,3 @@ def solve_problem(objectives_list, constraint_list):
         final_solution[key] = item.solution_value()
 
     return final_solution
-
-
-def get_json_from_file(file_path):
-    try:
-        with open(file_path, 'r') as fd:
-            json_decoded = json.load(fd)
-            for key in ["objectives", "constraints"]:
-                if key not in json_decoded:
-                    print('Error while reading the file %s, there is no %s key !' % key)
-                    exit(1)
-            return json_decoded
-    except json.JSONDecodeError:
-        print('Error while reading the file %s, it cannot be parsed to Json !' % file_path)
-        exit(1)
-
-
-def write_json_to_file(file_path, data):
-    with open(file_path, 'w') as fd:
-        json.dump(data, fd, indent=2)
-
-
-def main(file_path):
-    json_content = get_json_from_file(file_path)
-
-    solution = solve_problem(json_content['objectives'], json_content['constraints'])
-
-    json_content['solution'] = solution
-
-    write_json_to_file(file_path, json_content)
-
-
-if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        print("Need at least one arg")
-        exit(1)
-    if len(sys.argv) > 2:
-        print("Too many args")
-        exit(1)
-
-    main(sys.argv[1])
